f6e7052a1d334c3cbe9c3c07a1b0468b57e2081c
xialindeng <xialin.deng@thoughtworks.com>
2024-03-03 20:57:25 +0800

    init java project

---
+++ src/main/java/com/practice/travel/java_sc/JavaScApplication.java
@@ -0,0 +1,13 @@
+package com.practice.travel.java_sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaScApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/travel/java_sc/JavaScApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.travel.java_sc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
